a7cdd401ded073ff68bb3220975f90ad23e6c3b5
Chak Shing, Lo <ChakShing.Lo@justice.gov.uk>
2024-06-28 10:14:34 +0100

    update API gateway with demo config

---
+++ fpl-cafcass-api.tf
@@ -0,0 +1,73 @@
+module "fpl-cafcass-api-mgmt-product" {
+  source = "git@github.com:hmcts/cnp-module-api-mgmt-product?ref=master"
+
+  api_mgmt_name = local.api_mgmt_name
+  api_mgmt_rg   = local.api_mgmt_rg
+  name = var.fpl_cafcass_api_product_name
+  product_access_control_groups = ["developers"]
+  approval_required     = "false"
+  subscription_required = "true"
+  providers = {
+    azurerm = azurerm.aks-cftapps
+  }
+}
+
+module "fpl-cafcass-api-mgmt-api" {
+  source = "git@github.com:hmcts/cnp-module-api-mgmt-api?ref=master"
+
+  api_mgmt_name = local.api_mgmt_name
+  api_mgmt_rg   = local.api_mgmt_rg
+  revision      = "1"
+  service_url   = local.prl_api_url
+  product_id    = module.fpl-cafcass-api-mgmt-product.product_id
+  name          = "${var.fpl_cafcass_api_product_name}"
+  display_name  = "FPL Cafcass API"
+  path          = "public-law-api"
+  protocols     = ["http", "https"]
+  swagger_url   = "https://raw.githubusercontent.com/hmcts/cnp-api-docs/master/docs/specs/fpl-cafcass-api.json"
+
+  providers     = {
+    azurerm = azurerm.aks-cftapps
+  }
+}
+
+# data "template_file" "api_mgmt_policy_template" {
+#   template = file("${path.module}/template/api-policy.xml")
+#
+#   vars = {
+#     s2s_client_id                   = data.azurerm_key_vault_secret.s2s_client_id.value
+#     s2s_client_secret               = data.azurerm_key_vault_secret.s2s_client_secret.value
+#     s2s_base_url                    = local.s2sUrl
+#   }
+# }
+
+# module "prl-case-creation-policy" {
+#   source = "git@github.com:hmcts/cnp-module-api-mgmt-api-policy?ref=master"
+#
+#   api_mgmt_name = local.api_mgmt_name
+#   api_mgmt_rg   = local.api_mgmt_rg
+#
+#   api_name               = module.fpl-cafcass-api-mgmt-api.name
+#   api_policy_xml_content = data.template_file.api_mgmt_policy_template.rendered
+#
+#   providers     = {
+#     azurerm = azurerm.aks-cftapps
+#   }
+# }
+
+
+resource "azurerm_api_management_subscription" "fpl_cafcass_api_subscription" {
+  api_management_name = local.api_mgmt_name
+  resource_group_name = local.api_mgmt_rg
+  product_id          = module.fpl-cafcass-api-mgmt-product.id
+  display_name        = "FPL Cafcass API Subscription"
+  state               = "active"
+  provider            = azurerm.aks-cftapps
+
+}
+
+resource "azurerm_key_vault_secret" "fpl_cafcass_api_subscription_key" {
+  name         = "courtnav-subscription-sub-key"
+  value        = azurerm_api_management_subscription.fpl_cafcass_api_subscription.primary_key
+  key_vault_id = data.azurerm_key_vault.fis_key_vault.id
+}
---
+++ variables.tf
@@ -0,0 +1,50 @@
+# variable "product" {
+#   type = string
+#   default = "family"
+# }
+
+# variable "product_name" {
+#   type    = string
+#   default = "fpl-api"
+# }
+
+variable "fpl_cafcass_api_product_name" {
+  type    = string
+  default = "fpl-cafcass"
+}
+
+# variable "location" {
+#   type    = string
+#   default = "UK South"
+# }
+
+variable "env" {
+  type = string
+}
+
+# variable "tenant_id" {
+#   type        = string
+#   description = "(Required) The Azure Active Directory tenant ID that should be used for authenticating requests to the key vault. This is usually sourced from environemnt variables and not normally required to be specified."
+# }
+
+# variable "jenkins_AAD_objectId" {
+#   type        = string
+#   description = "(Required) The Azure AD object ID of a user, service principal or security group in the Azure Active Directory tenant for the vault. The object ID must be unique for the list of access policies."
+# }
+
+# thumbprint of the SSL certificate for API gateway tests
+# variable "api_gateway_test_certificate_thumbprints" {
+#   type    = list(any)
+#   default = [] # TODO: remove default and provide environment-specific values
+# }
+
+# variable "common_tags" {
+#   type = map(string)
+# }
+
+variable "aks_subscription_id" {
+}
+
+variable "apim_suffix" {
+  default = ""
+}
