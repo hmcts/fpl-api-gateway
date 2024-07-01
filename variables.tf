979705d9c2fb8fdea5035b44d5207015fc1a735a
Chak Shing, Lo <ChakShing.Lo@justice.gov.uk>
2024-07-01 17:22:08 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,7 +1,7 @@
-# variable "product" {
-#   type = string
-#   default = "family"
-# }
+variable "product" {
+  type = string
+  default = "fpl"
+}
 
 # variable "product_name" {
 #   type    = string
@@ -13,34 +13,34 @@ variable "fpl_cafcass_api_product_name" {
   default = "fpl-cafcass-api"
 }
 
-# variable "location" {
-#   type    = string
-#   default = "UK South"
-# }
+variable "location" {
+  type    = string
+  default = "UK South"
+}
 
 variable "env" {
   type = string
 }
 
-# variable "tenant_id" {
-#   type        = string
-#   description = "(Required) The Azure Active Directory tenant ID that should be used for authenticating requests to the key vault. This is usually sourced from environemnt variables and not normally required to be specified."
-# }
+variable "tenant_id" {
+  type        = string
+  description = "(Required) The Azure Active Directory tenant ID that should be used for authenticating requests to the key vault. This is usually sourced from environemnt variables and not normally required to be specified."
+}
 
-# variable "jenkins_AAD_objectId" {
-#   type        = string
-#   description = "(Required) The Azure AD object ID of a user, service principal or security group in the Azure Active Directory tenant for the vault. The object ID must be unique for the list of access policies."
-# }
+variable "jenkins_AAD_objectId" {
+  type        = string
+  description = "(Required) The Azure AD object ID of a user, service principal or security group in the Azure Active Directory tenant for the vault. The object ID must be unique for the list of access policies."
+}
 
-# thumbprint of the SSL certificate for API gateway tests
-# variable "api_gateway_test_certificate_thumbprints" {
-#   type    = list(any)
-#   default = [] # TODO: remove default and provide environment-specific values
-# }
+thumbprint of the SSL certificate for API gateway tests
+variable "api_gateway_test_certificate_thumbprints" {
+  type    = list(any)
+  default = [] # TODO: remove default and provide environment-specific values
+}
 
-# variable "common_tags" {
-#   type = map(string)
-# }
+variable "common_tags" {
+  type = map(string)
+}
 
 variable "aks_subscription_id" {
 }
