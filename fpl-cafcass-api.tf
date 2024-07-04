bde092eb5a68cabb5bc6110b3ad6f1d5e7fd2fe4
58060 <62423932+hmcts-jenkins-d-to-i[bot]@users.noreply.github.com>
2024-07-04 13:48:14 +0000

    Updating Terraform Formatting

--- fpl-cafcass-api.tf
+++ fpl-cafcass-api.tf
@@ -15,18 +15,18 @@ module "fpl-cafcass-api-mgmt-product" {
 module "fpl-cafcass-api-mgmt-api" {
   source = "git@github.com:hmcts/cnp-module-api-mgmt-api?ref=master"
 
-  api_mgmt_name         = local.api_mgmt_name
-  api_mgmt_rg           = local.api_mgmt_rg
-  revision              = "1"
-  service_url           = local.fpl_api_url
-  product_id            = module.fpl-cafcass-api-mgmt-product.product_id
-  name                  = join("-", [var.fpl_cafcass_api_product_name, "api"])
-  display_name          = "FPL Cafcass API"
-  path                  = "public-law-api"
-  protocols             = ["http", "https"]
-  swagger_url           = "https://raw.githubusercontent.com/hmcts/fpl-api-gateway/demo/template/fpl-cafcass-api.json?token=GHSAT0AAAAAACQWEJ35BNMZVJ3ZJP3U3DUCZUEAQHQ"
-#  subscription_required = "false"
-  content_format        = "openapi-link"
+  api_mgmt_name = local.api_mgmt_name
+  api_mgmt_rg   = local.api_mgmt_rg
+  revision      = "1"
+  service_url   = local.fpl_api_url
+  product_id    = module.fpl-cafcass-api-mgmt-product.product_id
+  name          = join("-", [var.fpl_cafcass_api_product_name, "api"])
+  display_name  = "FPL Cafcass API"
+  path          = "public-law-api"
+  protocols     = ["http", "https"]
+  swagger_url   = "https://raw.githubusercontent.com/hmcts/fpl-api-gateway/demo/template/fpl-cafcass-api.json?token=GHSAT0AAAAAACQWEJ35BNMZVJ3ZJP3U3DUCZUEAQHQ"
+  #  subscription_required = "false"
+  content_format = "openapi-link"
 
   providers = {
     azurerm = azurerm.aks-cftapps
@@ -58,18 +58,18 @@ module "fpl-cafcass-api-policy" {
   }
 }
 
- resource "azurerm_api_management_subscription" "fpl_cafcass_api_subscription" {
-   api_management_name = local.api_mgmt_name
-   resource_group_name = local.api_mgmt_rg
-   product_id          = module.fpl-cafcass-api-mgmt-product.id
-   display_name        = "FPL Cafcass API Subscription"
-   state               = "active"
-   provider            = azurerm.aks-cftapps
+resource "azurerm_api_management_subscription" "fpl_cafcass_api_subscription" {
+  api_management_name = local.api_mgmt_name
+  resource_group_name = local.api_mgmt_rg
+  product_id          = module.fpl-cafcass-api-mgmt-product.id
+  display_name        = "FPL Cafcass API Subscription"
+  state               = "active"
+  provider            = azurerm.aks-cftapps
 
- }
+}
 
- resource "azurerm_key_vault_secret" "fpl_cafcass_api_subscription_key" {
-   name         = "fpl-cafcass-api-subscription-key"
-   value        = azurerm_api_management_subscription.fpl_cafcass_api_subscription.primary_key
-   key_vault_id = data.azurerm_key_vault.fpl_key_vault.id
- }
+resource "azurerm_key_vault_secret" "fpl_cafcass_api_subscription_key" {
+  name         = "fpl-cafcass-api-subscription-key"
+  value        = azurerm_api_management_subscription.fpl_cafcass_api_subscription.primary_key
+  key_vault_id = data.azurerm_key_vault.fpl_key_vault.id
+}
