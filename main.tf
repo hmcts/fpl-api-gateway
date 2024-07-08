9af05ec8b15d170a84d77bebe5e68c7770137fb6
Chak Shing, Lo <ChakShing.Lo@justice.gov.uk>
2024-07-08 10:00:15 +0100

    update secret and idam policy

--- main.tf
+++ main.tf
@@ -3,6 +3,10 @@ locals {
   api_mgmt_name   = "cft-api-mgmt-${local.api_mgmt_suffix}"
   api_mgmt_rg     = join("-", ["cft", var.env, "network-rg"])
 
+  idam_url    = var.env == "prod" ? "https://hmcts-access.service.gov.uk" : "https://idam-web-public.${var.env}.platform.hmcts.net"
+  idam_env    = var.env == "prod" || var.env == "aat" ? "${var.env}2" : var.env
+  oidc_issuer = "https://forgerock-am.service.core-compute-idam-${local.idam_env}.internal:8443/openam/oauth2/realms/root/realms/hmcts"
+
   fpl_key_vault    = join("-", ["fpl", var.env])
   fpl_key_vault_rg = join("-", ["fpl-case-service", var.env])
 
@@ -23,13 +27,13 @@ data "azurerm_key_vault" "fpl_key_vault" {
 }
 
 
-#data "azurerm_key_vault_secret" "s2s_client_id" {
-#  name         = "gateway-s2s-client-id"
-#  key_vault_id = data.azurerm_key_vault.fpl_key_vault.id
-#}
+data "azurerm_key_vault_secret" "s2s_client_id" {
+  name         = "gateway-s2s-client-id"
+  key_vault_id = data.azurerm_key_vault.fpl_key_vault.id
+}
 
 data "azurerm_key_vault_secret" "s2s_client_secret" {
-  #  name         = "gateway-s2s-client-secret"
-  name         = "fpl-case-service-s2s-secret"
+    name         = "gateway-s2s-client-secret"
+#  name         = "fpl-case-service-s2s-secret"
   key_vault_id = data.azurerm_key_vault.fpl_key_vault.id
 }
